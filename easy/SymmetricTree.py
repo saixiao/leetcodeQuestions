19c686ac04cceb8af3cb63c29273d95def64a5c0
Sai Xiao <s28xiao@edu.uwaterloo.ca>
2021-09-04 15:04:20 -0700

    some BT questions

---
+++ easy/SymmetricTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        
+        return self.dfs(root.left, root.right)
+        
+        
+        
+    def dfs(self, root1, root2):
+        
+        # both None
+        if root1 == None == root2:
+            return True
+        elif root1 == None or root2 == None:
+            return False
+        elif root1.val != root2.val:
+            return False
+        
+        return self.dfs(root1.right, root2.left) and self.dfs(root1.left, root2.right)
