7e64b8f2cc8131d06a09483d016f384579d4abff
Sai Xiao <s28xiao@edu.uwaterloo.ca>
2021-09-06 21:40:39 -0700

    improved 3sum

--- medium/other/3sumClosest.py
+++ medium/other/3sumClosest.py
@@ -1,49 +1,21 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        if len(nums) == 3:
-            return sum(nums)
-        closest = float("inf")
-        result = []
         nums.sort()
+        result = nums[0] + nums[1] + nums[2]
         
         for i in range(len(nums)):
-            if closest == 0:
-                return sum(result)
-
-            res, c = self.twoSum(closest, target - nums[i], i, nums, nums[i])
-
-            if c < closest:
-                result = res
-                closest = c
-        
-        return sum(result)
-    
-    def twoSum(self, closest, target, i, nums, current_number):
-        lptr = 0
-        rptr = len(nums) - 1
-        result = []
-        
-        while lptr < rptr:
-            if lptr == i:
-                lptr += 1
-                continue
-            elif rptr == i:
-                rptr -= 1
-                continue
-            
-            curr_sum = nums[lptr] + nums[rptr]
-            
-            if abs(target - curr_sum) == 0:
-                return [current_number, nums[lptr], nums[rptr]], 0
-
-            if abs(target - curr_sum) < closest:
-                closest = abs(target - curr_sum)
-                result = [current_number, nums[lptr], nums[rptr]]
-            
-            if target - curr_sum > 0:
-                lptr += 1
-            elif target - curr_sum < 0:
-                rptr -= 1
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == target:
+                    return s
                 
-        return result, closest
+                if abs(target - result) > abs(target - s):
+                    result = s
                 
+                if s > target:
+                    k -= 1
+                elif s < target:
+                    j += 1
+        
+        return result
