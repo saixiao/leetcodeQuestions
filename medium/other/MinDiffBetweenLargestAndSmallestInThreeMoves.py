6ad6787a73dcb2ef938d52e79b4e4c3a451bdb3b
Sai Xiao <s28xiao@edu.uwaterloo.ca>
2021-09-28 12:20:32 -0400

    improved min diff

--- medium/other/MinDiffBetweenLargestAndSmallestInThreeMoves.py
+++ medium/other/MinDiffBetweenLargestAndSmallestInThreeMoves.py
@@ -1,4 +1,4 @@
 class Solution:
-    def minDifference(self, nums: List[int]) -> int:
+    def minDifference(self, nums: List[int], int: k) -> int:
         nums.sort()
-        return min((b - a for a,b in zip(nums[:4], nums[-4:])))
+        return min((b - a for a,b in zip(nums[:k], nums[-k:])))
